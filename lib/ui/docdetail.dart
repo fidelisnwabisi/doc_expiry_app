19e1aa6ffb56cdf47be30b3ee2b9556f5ae344b6
fidelis <fidelisnwabisi@gmail.com>
2023-08-10 09:51:59 +0100

    added  the initialization code for docdetail

--- lib/ui/docdetail.dart
+++ lib/ui/docdetail.dart
@@ -1,7 +1,9 @@
-import 'package:doc_expiry_app/model/model.dart';
-import 'package:doc_expiry_app/util/dbhelper.dart';
+import 'package:doc_expiry_app/util/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text2/flutter_masked_text2.dart';
+
+import '../model/model.dart';
+import '../util/dbhelper.dart';
 
 const menuDelete = "Delete";
 final List<String> menuOptions = const <String>[menuDelete];
@@ -35,5 +37,23 @@ class DocDetailState extends State<DocDetail> {
   Widget build(Object context) {
     // TODO: implement build
     throw UnimplementedError();
+  }
+
+  // Initialization code
+  void _initCtrls() {
+    titleCtrl.text = widget.doc.title != null ? widget.doc.title : "";
+    expirationCtrl.text =
+        widget.doc.expiration != null ? widget.doc.expiration : "";
+
+    fqYearCtrl =
+        widget.doc.fqYear != null ? Val.IntToBool(widget.doc.fqYear) : false;
+    fqHalfYearCtrl = widget.doc.fqHalfYear != null
+        ? Val.IntToBool(widget.doc.fqHalfYear)
+        : false;
+    fqQuarterCtrl = widget.doc.fqQuarter != null
+        ? Val.IntToBool(widget.doc.fqQuarter)
+        : false;
+    fqMonthCrtl =
+        widget.doc.fqMonth != null ? Val.IntToBool(widget.doc.fqMonth) : false;
   }
 }
