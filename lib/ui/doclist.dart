9ace198396d54fdd580cb4da90cf77ab905ec3b6
fidelis <fidelisnwabisi@gmail.com>
2023-08-11 19:54:46 +0100

    added more functions in the doclist.dart

--- lib/ui/doclist.dart
+++ lib/ui/doclist.dart
@@ -26,9 +26,105 @@ class DocListState extends State<DocList> {
     super.initState();
   }
 
+  Future getData() async {
+    final dbFuture = dbh.initializeDb();
+    dbFuture.then(
+        // result here is the actual reference to the database object.
+        (result) {
+      final docsFuture = dbh.getDocs();
+      docsFuture.then(
+          // result here is the actual list of docs in the database.
+          (result) {
+        if (result.length >= 0) {
+          List<Doc> docList = [];
+          var count = result.length;
+          for (int i = 0; i <= count - 1; i++) {
+            docList.add(Doc.fromObject(result[i]));
+          }
+          setState(() {
+            if (this.docs.length > 0) {
+              this.docs.clear();
+            }
+            this.docs = docList;
+            this.count = count;
+          });
+        }
+      });
+    });
+  }
+
+  void _checkDate() {
+    const secs = const Duration(seconds: 10);
+
+    new Timer.periodic(secs, (Timer t) {
+      DateTime nw = DateTime.now();
+
+      if (cDate.day != nw.day ||
+          cDate.month != nw.month ||
+          cDate.year != nw.year) {
+        getData();
+        cDate = DateTime.now();
+      }
+    });
+  }
+
+  void navigateToDetail(Doc doc) async {
+    bool r = await Navigator.push(
+        context, MaterialPageRoute(builder: (context) => DocDetail(doc)));
+
+    if (r == true) {
+      getData();
+    }
+  }
+
+  void _showResetDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Text("Reset"),
+            content: new Text("Do you want to delete all the local data"),
+            actions: <Widget>[
+              TextButton(
+                child: new Text("Cancel"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: new Text("OK"),
+                onPressed: () {
+                  Future f = _resetLocalData();
+                  f.then((result) {
+                    Navigator.of(context).pop();
+                  });
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  Future _resetLocalData() async {
+    final dbFuture = dbh.initializeDb();
+    dbFuture.then((result) {
+      final dDocs = dbh.deleteRows(DbHelper.tblDocs);
+      dDocs.then((result) {
+        setState(() {
+          this.docs.clear();
+          this.count = 0;
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
   }
 }
+
+
+// The class 'List' doesn't have an unnamed constructor.
+// Try using one of the named constructors defined in 'List'.
